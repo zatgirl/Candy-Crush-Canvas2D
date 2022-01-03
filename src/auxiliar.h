490ef96278974bf0a9f99796119dddf82df8d4bf
zatgirl <59773787+zatgirl@users.noreply.github.com>
2022-01-03 07:26:46 -0300

    att

--- src/auxiliar.h
+++ src/auxiliar.h
@@ -1,6 +1,31 @@
+#ifndef __AUXILIAR_H__
+#define __AUXILIAR_H__
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include "gl_canvas2d.h"
+
+class orange{
+public:
+    //CV::rectFill();
+};
+class blue{
+public:
+    //CV::rectFill();
+};
+class green{
+public:
+    //CV::rectFill();
+};
+class centroidPieces{
+public:
+    int centerX[13][13] = {{0}};
+    int centerY[13][13] = {{0}};
+    int centerColor[13][13] = {{0}};
+};
 
 
-void funcaoDeOutroArquivo()
-{
-   // printf("\nSou funcao em outro arquivo");
-}
+#endif // __AUXILIAR_H__
--- src/main.cpp
+++ src/main.cpp
@@ -21,19 +21,82 @@
 #include <stdlib.h>
 
 #include "gl_canvas2d.h"
-
 #include "auxiliar.h"
 
+#define tray 13 //Tamanho do tabuleiro definido em 13x13
+#define pieceSize 50 //Tamanho de cada peça em px
+#define borderTray 10
+
 //variaveis globais
-int screenWidth = 793, screenHeight = 793; //largura e altura inicial da tela. Alteram com o redimensionamento de tela.
-int mouseX, mouseY; //variaveis globais do mouse para poder exibir dentro da render().
+int screenWidth = 683, screenHeight = 633; //largura e altura inicial da tela. Alteram com o redimensionamento de tela.
+int mouseX, mouseY, stateMouse; //variaveis globais do mouse para poder exibir dentro da render().
+int passo = 0 ;
+
+orange*candy = new orange();
+centroidPieces*piece = new centroidPieces();
+
+void base_matrix(){
+   float x1,x2,y1,y2;
+   for (int line = 0 ; line < tray ; line++){
+       for (int col = 0 ; col < tray ; col++){
+            CV::color(0,0,0);
+            x1 = borderTray+(pieceSize*(line+1));
+            y1 = borderTray+(pieceSize*col);
+            x2 = borderTray+(pieceSize*line);
+            y2 = borderTray+(pieceSize*line);
+            CV::rect(x1,y1,x2,y2);
+            //printf("[%d][%d] => x1=%.2f, x2=%.2f, y1=%.2f, y2=%.2f\n", line,col,x1,x2,y1,y2);
+            piece->centerX[line][col] = (x1-(pieceSize/2));
+            piece->centerY[line][col] = (y1-(pieceSize/2));
+            if(passo == 0){
+                piece->centerColor[line][col] = (1+(rand()%4));
+            }
+            //printf("%d, %d\n", piece->centerX[line][col], piece->centerY[line][col]);
+           // CV::color(1,0,0);
+           // CV::circleFill(piece->centerX[line][col], piece->centerY[line][col], 5, 20);
+       }
+   }passo++;
 
 
-//funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis
-//globais que podem ser setadas pelo metodo keyboard()
+}
+
+void Candys(){
+    int k;
+    for (int line = 1; line < 12; line++){
+       for (int col = 2; col < 11; col++){
+            k = piece->centerColor[line][col];
+            if (k == 1) {
+                CV::color(0,0,1);
+                CV::circleFill(piece->centerX[line][col], piece->centerY[line][col], 15, 20);
+            }
+            if (k == 2) {
+                CV::color(1,0.5,1);
+                CV::circleFill(piece->centerX[line][col], piece->centerY[line][col], 15, 20);
+            }
+            if (k == 3) {
+                CV::color(0,1,0);
+                CV::circleFill(piece->centerX[line][col], piece->centerY[line][col], 15, 20);
+            }
+            if (k == 4) {
+                CV::color(1,0,0);
+                CV::circleFill(piece->centerX[line][col], piece->centerY[line][col], 15, 20);
+            }
+
+        }
+    }
+}
+
+void teste(){
+    float dist = sqrt((x-mx)*(x))
+    if (stateMouse == 1){
+
+   }
+}
+
 void render()
 {
-
+    base_matrix();
+    Candys();
 }
 
 //funcao chamada toda vez que uma tecla for pressionada
@@ -53,9 +116,9 @@ void keyboardUp(int key)
 void mouse(int button, int state, int wheel, int direction, int x, int y)
 {
    printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
-
-   mouseX = x; //guarda as coordenadas do mouse para exibir dentro da render()
-   mouseY = y;
+        mouseX = x;
+        mouseY = y;
+        stateMouse = state;
 }
 
 int main(void)
